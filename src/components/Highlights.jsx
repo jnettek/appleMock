3e7b01ffb2fe5d546acc4f22aaacb9372ab1da2e
jnettek <jnettekpelletier@live.com>
2024-04-12 22:59:01 -0400

    first commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,32 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap/gsap-core'
+import React from 'react'
+import { watchImg, rightImg } from '../utils';
+import VideoCarousel from './VideoCarousel';
+
+export const Highlights = () => {
+  useGSAP(()=> {
+    gsap.to('#title', {opacity: 1, y: 0})
+    //stagger: 0.25 means that each element will be delayed by 0.25 seconds
+    gsap.to('.link', {opacity: 1, y: 0, duration: 1, stagger: 0.25})
+  },[])
+
+  return (
+    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className='screen-max-width'>
+        <div className='mb-12 w-full items-end md:flex justify-between'>
+          <h1 id='title' className='section-heading'>Get the highlights.</h1>
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link'>watch the film
+            <img src={watchImg} alt="watch" className='ml-2'/>
+            </p>
+            <p className='link'>watch the film
+            <img src={rightImg} alt="right" className='ml-2'/>
+            </p>
+          </div>
+        </div>
+        <VideoCarousel/>
+      </div>
+    </section>
+  )
+}
